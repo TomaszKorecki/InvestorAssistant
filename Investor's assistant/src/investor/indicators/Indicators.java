e4b6aa520db3e8c95e5791c5c72f3c0c2f2e8ae2
Daniel <Daniel@Daniel>
2014-11-15 16:48:47 +0100

    Narazie 3 wskaźniki, dodatkowe 2 (Kopera i śednia wyładnicza)

--- Investor's assistant/src/investor/indicators/Indicators.java
+++ Investor's assistant/src/investor/indicators/Indicators.java
@@ -29,7 +29,7 @@ public class Indicators{
                 for(int j=i;j<i+n;j++){
                     avg_close+=data[j].getClose_val();
                 }
-                avg_close/=n;
+                avg_close=avg_close/n;
                 out[i]=avg_close;
             }
         }
@@ -47,13 +47,13 @@ public class Indicators{
                 for(int j=i;j<data.length;j++){
                     sum=sum+Math.pow(data[j].getClose_val()-temp[i],2);
                 }
-                out[i]=Math.sqrt(sum/nn);
+                out[i]=Math.sqrt(sum/(double)nn);
             }else{
                 double sum=0;
                 for(int j=i;j<i+n;j++){
                     sum=sum+Math.pow(data[j].getClose_val()-temp[i],2);
                 }
-                out[i]=Math.sqrt(sum/n);
+                out[i]=Math.sqrt(sum/(double)n);
             }
         }
         return out;
@@ -68,6 +68,48 @@ public class Indicators{
         for(int i=0;i<data.length;i++){
             out[0][i]=out[1][i]+(temp[i]*k);
             out[2][i]=out[1][i]-(temp[i]*k);
+        }
+        return out;
+    }
+    //Wskaźnik - kopera, podobne do Wstęgi Bollingera, ale tym razem odległośćod od MA jest procentowa, nie
+    //K-krotnością odchylenia standardowego. Nie wymaga także wyświetlania śedniej kroczącej, dlatego
+    //zwracana macierz jest 2xn,kolumna o indeksie 0 to górna linia, 1 dolna
+    //Wartość p zwykle ustalana na poziomie 2,5-10%
+    public double[][] Koperta(Index[] data,int n,int p){
+        double[][] out = new double[2][data.length];
+        double[] temp = new double[data.length];
+        temp=MA(data,n);
+         for(int i=0;i<data.length;i++){
+            out[0][i]=temp[i]+((double)p/100)*temp[i];
+            out[1][i]=temp[i]-((double)p/100)*temp[i];
+        }
+        return out;
+    }
+    //Wykładnicza średnia ruchoma (EMA)
+    //Podobne do średniej, jednak każda kolejna wartość brana pod uwagę do śedniej posada wykładniczo mniejszą
+    //wagę (współczynnik jest równy (1-alfa)^n gdzie n to kolejna cena brana pod uwage przy wyliczaniu aktualenej ceny
+    public double[] EMA(Index[] data,int n,double alfa){
+        double[] out = new double[data.length];
+        for(int i=0;i<data.length;i++){
+            int expo=0;
+            double avg_close=0;
+            double div=0;
+            if(data.length-n<=i){
+                 for(int j=i;j<data.length;j++){
+                    avg_close+=data[j].getClose_val()*Math.pow((1-alfa),expo);
+                    div+=Math.pow((1-alfa), expo);
+                    expo++;
+                }
+                out[i]=avg_close/div;
+                System.out.println();
+            }else{
+                for(int j=i;j<i+n;j++){
+                    avg_close+=data[j].getClose_val()*Math.pow((1-alfa),expo);
+                    div+=Math.pow((1-alfa), expo);
+                    expo++;
+                }
+                out[i]=avg_close/div;
+            }
         }
         return out;
     }
