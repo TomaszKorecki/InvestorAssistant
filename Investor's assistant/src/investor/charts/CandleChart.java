da3387938cafd941293900dcc6cdde6efdfa0f96
mniemiec <michaln92@gmail.com>
2014-11-16 14:23:05 +0100

    Metoda generująca dane dla wykresu świecowego na podstawie tablicy obiektów Index

--- Investor's assistant/src/investor/charts/CandleChart.java
+++ Investor's assistant/src/investor/charts/CandleChart.java
@@ -30,6 +30,7 @@ import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import investor.data.Index;
 
 /**
  *
@@ -37,39 +38,43 @@ import javafx.util.Duration;
  */
 public class CandleChart {
     // DAY, OPEN, CLOSE, HIGH, LOW, AVERAGE
-    private static double[][] data = new double[][]{
-            {1,  25, 20, 32, 16, 20},
-            {2,  26, 30, 33, 22, 25},
-            {3,  30, 38, 40, 20, 32},
-            {4,  24, 30, 34, 22, 30},
-            {5,  26, 36, 40, 24, 32},
-            {6,  28, 38, 45, 25, 34},
-            {7,  36, 30, 44, 28, 39},
-            {8,  30, 18, 36, 16, 31},
-            {9,  40, 50, 52, 36, 41},
-            {10, 30, 34, 38, 28, 36},
-            {11, 24, 12, 30, 8,  32.4},
-            {12, 28, 40, 46, 25, 31.6},
-            {13, 28, 18, 36, 14, 32.6},
-            {14, 38, 30, 40, 26, 30.6},
-            {15, 28, 33, 40, 28, 30.6},
-            {16, 25, 10, 32, 6,  30.1},
-            {17, 26, 30, 42, 18, 27.3},
-            {18, 20, 18, 30, 10, 21.9},
-            {19, 20, 10, 30, 5,  21.9},
-            {20, 26, 16, 32, 10, 17.9},
-            {21, 38, 40, 44, 32, 18.9},
-            {22, 26, 40, 41, 12, 18.9},
-            {23, 30, 18, 34, 10, 18.9},
-            {24, 12, 23, 26, 12, 18.2},
-            {25, 30, 40, 45, 16, 18.9},
-            {26, 25, 35, 38, 20, 21.4},
-            {27, 24, 12, 30, 8,  19.6},
-            {28, 23, 44, 46, 15, 22.2},
-            {29, 28, 18, 30, 12, 23},
-            {30, 28, 18, 30, 12, 23.2},
-            {31, 28, 18, 30, 12, 22}
-    };
+    
+    private static double[][] data;
+    
+    
+//    private static double[][] data = new double[][]{
+//            {1,  25, 20, 32, 16, 20},
+//            {2,  26, 30, 33, 22, 25},
+//            {3,  30, 38, 40, 20, 32},
+//            {4,  24, 30, 34, 22, 30},
+//            {5,  26, 36, 40, 24, 32},
+//            {6,  28, 38, 45, 25, 34},
+//            {7,  36, 30, 44, 28, 39},
+//            {8,  30, 18, 36, 16, 31},
+//            {9,  40, 50, 52, 36, 41},
+//            {10, 30, 34, 38, 28, 36},
+//            {11, 24, 12, 30, 8,  32.4},
+//            {12, 28, 40, 46, 25, 31.6},
+//            {13, 28, 18, 36, 14, 32.6},
+//            {14, 38, 30, 40, 26, 30.6},
+//            {15, 28, 33, 40, 28, 30.6},
+//            {16, 25, 10, 32, 6,  30.1},
+//            {17, 26, 30, 42, 18, 27.3},
+//            {18, 20, 18, 30, 10, 21.9},
+//            {19, 20, 10, 30, 5,  21.9},
+//            {20, 26, 16, 32, 10, 17.9},
+//            {21, 38, 40, 44, 32, 18.9},
+//            {22, 26, 40, 41, 12, 18.9},
+//            {23, 30, 18, 34, 10, 18.9},
+//            {24, 12, 23, 26, 12, 18.2},
+//            {25, 30, 40, 45, 16, 18.9},
+//            {26, 25, 35, 38, 20, 21.4},
+//            {27, 24, 12, 30, 8,  19.6},
+//            {28, 23, 44, 46, 15, 22.2},
+//            {29, 28, 18, 30, 12, 23},
+//            {30, 28, 18, 30, 12, 23.2},
+//            {31, 28, 18, 30, 12, 22}
+//    };
 
 //    public void init(Stage primaryStage) {
 //        Group root = new Group();
@@ -82,9 +87,25 @@ public class CandleChart {
 //        
 //        root.getChildren().add(createChart());
 //    }
+    
+    
+    //metoda przyjmująca tablicę indexów, generująca dane dla wykresu
+    public static void generateData(Index[] indexData) {
+        data = new double[indexData.length][6];
+        for (int i = 0; i < indexData.length; i++) {
+            data[i][0] = Double.parseDouble(indexData[i].getDay());
+            data[i][1] = indexData[i].getOpen_val();
+            data[i][2] = indexData[i].getClose_val();
+            data[i][3] = indexData[i].getMin_val();
+            data[i][4] = indexData[i].getMax_val();
+            data[i][5] = indexData[i].getVol_val();
+        }        
+    }
 
     public CandleStickChart createChart() {
-        final NumberAxis xAxis = new NumberAxis(0,32,1);
+        final NumberAxis xAxis = new NumberAxis(0,data.length+1,1);
+        
+//        final NumberAxis xAxis = new NumberAxis(0,32,1);
         xAxis.setMinorTickCount(0);
         final NumberAxis yAxis = new NumberAxis();
         final CandleStickChart bc = new CandleStickChart(xAxis,yAxis);
@@ -255,10 +276,10 @@ public class CandleChart {
                 }
             }
             // create series path
-//            Path seriesPath = new Path();
-//            seriesPath.getStyleClass().setAll("candlestick-average-line", "series" + seriesIndex);
-//            series.setNode(seriesPath);
-//            getPlotChildren().add(seriesPath);
+            Path seriesPath = new Path();
+            seriesPath.getStyleClass().setAll("candlestick-average-line", "series" + seriesIndex);
+            series.setNode(seriesPath);
+            getPlotChildren().add(seriesPath);
         }
 
         @Override protected void seriesRemoved(XYChart.Series<Number, Number> series) {
