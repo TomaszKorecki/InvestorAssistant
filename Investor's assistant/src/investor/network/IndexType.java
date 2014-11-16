0a26a21b77ab46b508c558c45b0aea38f65be736
Tomasz <tomasz-korecki@wp.pl>
2014-11-16 11:35:11 +0100

    Klasa z enumem indeksow gieldowych

---
+++ Investor's assistant/src/investor/network/IndexType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package investor.network;
+
+/**
+ *
+ * @author Tomasz
+ */
+public enum IndexType {
+    AEX, BEL20, CAC, DAX, IBEX, OMXR, OMXS, OMXT, OMXV, OSEAX, SMI, WIG20, DJI, NDQ, NDX, NKX, SHC, STI, HSI, ACR
+}
